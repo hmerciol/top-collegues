83ba54d357bb284d555e6128615c3bf02bc75752
hmerciol <li.henri@merciol.fr>
2018-02-14 17:11:21 +0100

    avatar par defaut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,26 +9,29 @@ import { Collegue } from './shared/domain/collegue';
 export class AppComponent implements OnInit {
   collegues:Collegue[];
   added:boolean;
+  wrong:boolean;
   title = 'app';
 
   add(pseudo:HTMLInputElement, imageUrl: HTMLInputElement) {
-    if(pseudo.value == '' || imageUrl.value == ''){
+    if(pseudo.value == ''){
+      this.wrong=true;
       this.added=false;
     }else{
       this.collegues.push(new Collegue(pseudo.value,imageUrl.value,0));
       pseudo.value='';
       imageUrl.value='';
       this.added=true;
+      this.wrong=false;
     }
     return false; // pour éviter le rechargement de la page
   }
 
   ngOnInit() {
     this.collegues = new Array;
-    this.collegues.push(new Collegue('Jean','TODO',95));
-    this.collegues.push(new Collegue('Jacques','TODO',75));
-    this.collegues.push(new Collegue('Josseline','TODO',70));
-    this.collegues.push(new Collegue('Jérémi','TODO',90));
-    this.collegues.push(new Collegue('Justine','TODO',100));
+    this.collegues.push(new Collegue('Jean','',95));
+    this.collegues.push(new Collegue('Jacques','',75));
+    this.collegues.push(new Collegue('Josseline','',70));
+    this.collegues.push(new Collegue('Jérémi','',90));
+    this.collegues.push(new Collegue('Justine','',100));
   }
 }
