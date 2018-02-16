30a6c6b10106234744936eb6afae7702bd2ad21b
hmerciol <li.henri@merciol.fr>
2018-02-16 15:27:39 +0100

    TP routes : vue carrousel ajoutée

--- src/app/vue-carrousel/vue-carrousel.component.ts
+++ src/app/vue-carrousel/vue-carrousel.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { VueAbstraite } from '../shared/domain/vue-abstraite';
+import { CollegueService } from '../shared/service/collegue.service';
 
 @Component({
   selector: 'app-vue-carrousel',
   templateUrl: './vue-carrousel.component.html',
   styleUrls: ['./vue-carrousel.component.css']
 })
-export class VueCarrouselComponent implements OnInit {
+export class VueCarrouselComponent extends VueAbstraite {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(colService:CollegueService){
+      super(colService)
   }
 
 }
