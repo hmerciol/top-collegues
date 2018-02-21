1848412e62e2ddacf964e7ff5edad65dc8e1c3c7
hmerciol <li.henri@merciol.fr>
2018-02-21 17:05:57 +0100

    tp rs - affichage des commentaires

--- src/app/vue-detail/vue-detail.component.ts
+++ src/app/vue-detail/vue-detail.component.ts
@@ -5,6 +5,8 @@ import { Collegue } from '../shared/domain/collegue';
 import { BouttonsCollegue, Status } from '../shared/domain/bouttons-collegue';
 import { Router } from '@angular/router';
 import { AvisService } from '../shared/service/avis.service';
+import { Commentaire } from '../shared/domain/commentaire';
+import { CommentaireService } from '../shared/service/commentaire.service';
 
 @Component({
   selector: 'app-vue-detail',
@@ -13,11 +15,20 @@ import { AvisService } from '../shared/service/avis.service';
 })
 export class VueDetailComponent extends BouttonsCollegue {
 
-  constructor(private router: Router, private route: ActivatedRoute, colService:CollegueService,public voteService:AvisService) {
+  comments:Commentaire[];
+
+  constructor(private router: Router, private route: ActivatedRoute, colService:CollegueService,public voteService:AvisService,public commService:CommentaireService) {
     super(colService,voteService)
     route.params.subscribe(params => {
       colService.trouverUnCollegue(params['pseudo'])
-      .subscribe(collegue => this.tmpCollegue = collegue)
+      .subscribe(collegue => {
+        this.tmpCollegue = collegue;
+        commService.listerCommentaire(collegue)
+        .subscribe(result => {
+          this.comments = result;
+          this.comments.forEach(comm => console.log(comm.comment))
+        })
+      })
     });
   }
 
